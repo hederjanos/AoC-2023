6a04a1c1dc85c51d76fcb248957493630d2881ce
hederjanos <hederjanos@gmail.com>
2026-08-23 18:21:28 +0200

    revisit day1

--- java-module/src/main/java/day/_1/Day1Solver.java
+++ java-module/src/main/java/day/_1/Day1Solver.java
@@ -2,9 +2,6 @@ package day._1;
 
 import util.common.Solver;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Day1Solver extends Solver<Integer> {
     public Day1Solver(String filename) {
         super(filename);
@@ -22,51 +19,57 @@ public class Day1Solver extends Solver<Integer> {
 
     private record CalibrationValue(int value) {
         static CalibrationValue from(String line, boolean withSpelled) {
-            List<Integer> digits = new ArrayList<>();
-            int i = 0;
-            while (i < line.length()) {
-                boolean matchedWithSpelled = false;
-                char charAt = line.charAt(i);
-                if (Character.isDigit(charAt)) {
-                    digits.add(Character.getNumericValue(charAt));
+            int first = -1;
+            int last = -1;
+
+            for (int i = 0; i < line.length(); i++) {
+                char c = line.charAt(i);
+                int currentDigit = -1;
+
+                if (Character.isDigit(c)) {
+                    currentDigit = c - '0';
                 } else if (withSpelled) {
-                    String substring = line.substring(i);
-                    for (Digit digit : Digit.values()) {
-                        if (substring.startsWith(digit.name().toLowerCase())) {
-                            digits.add(digit.getValue());
-                            i += digit.name().length() - 1;
-                            matchedWithSpelled = true;
-                            break;
-                        }
-                    }
+                    currentDigit = Digit.matchAt(line, i);
                 }
-                if (!matchedWithSpelled) {
-                    i++;
+
+                if (currentDigit != -1) {
+                    if (first == -1) {
+                        first = currentDigit;
+                    }
+                    last = currentDigit;
                 }
             }
-            return new CalibrationValue(Integer.parseInt(digits.get(0) + "" + digits.get(digits.size() - 1)));
+
+            return new CalibrationValue(first * 10 + last);
         }
     }
 
     private enum Digit {
-        ONE(1),
-        TWO(2),
-        THREE(3),
-        FOUR(4),
-        FIVE(5),
-        SIX(6),
-        SEVEN(7),
-        EIGHT(8),
-        NINE(9);
+        ONE(1, "one"),
+        TWO(2, "two"),
+        THREE(3, "three"),
+        FOUR(4, "four"),
+        FIVE(5, "five"),
+        SIX(6, "six"),
+        SEVEN(7, "seven"),
+        EIGHT(8, "eight"),
+        NINE(9, "nine");
 
         final int value;
+        final String text;
 
-        Digit(int value) {
+        Digit(int value, String text) {
             this.value = value;
+            this.text = text;
         }
 
-        int getValue() {
-            return value;
+        static int matchAt(String line, int index) {
+            for (Digit digit : values()) {
+                if (line.startsWith(digit.text, index)) {
+                    return digit.value;
+                }
+            }
+            return -1;
         }
     }
 }
--- kotlin-module/src/main/kotlin/Day1.kt
+++ kotlin-module/src/main/kotlin/Day1.kt
@@ -11,52 +11,50 @@ class Day1SolverK(
 
     override fun solvePartTwo(): Int = puzzle.sumOf { line -> CalibrationValue.from(line, withSpelled = true).value }
 
-    private class CalibrationValue(
-        val value: Int,
-    ) {
+    private class CalibrationValue(val value: Int) {
         companion object {
-            fun from(
-                line: String,
-                withSpelled: Boolean = false,
-            ): CalibrationValue {
-                val digits = mutableListOf<Int>()
-                var i = 0
-                while (i < line.length) {
-                    var matchedWithSpelled = false
-                    val charAt = line[i]
-                    if (charAt.isDigit()) {
-                        digits.add(charAt.digitToInt())
+            fun from(line: String, withSpelled: Boolean = false): CalibrationValue {
+                var first = -1
+                var last = -1
+
+                for (i in line.indices) {
+                    val c = line[i]
+                    var currentDigit = -1
+
+                    if (c.isDigit()) {
+                        currentDigit = c.digitToInt()
                     } else if (withSpelled) {
-                        val substring = line.substring(i)
-                        for (digit in Digit.entries) {
-                            if (substring.startsWith(digit.name.lowercase())) {
-                                digits.add(digit.value)
-                                i += digit.name.length - 1
-                                matchedWithSpelled = true
-                                break
-                            }
-                        }
+                        currentDigit = Digit.matchAt(line, i)
                     }
-                    if (!matchedWithSpelled) {
-                        i++
+
+                    if (currentDigit != -1) {
+                        if (first == -1) {
+                            first = currentDigit
+                        }
+                        last = currentDigit
                     }
                 }
-                return CalibrationValue("${digits.first()}${digits.last()}".toInt())
+
+                return CalibrationValue(first * 10 + last)
             }
         }
     }
 
-    private enum class Digit(
-        val value: Int,
-    ) {
-        ONE(1),
-        TWO(2),
-        THREE(3),
-        FOUR(4),
-        FIVE(5),
-        SIX(6),
-        SEVEN(7),
-        EIGHT(8),
-        NINE(9),
+    private enum class Digit(val value: Int, val text: String) {
+        ONE(1, "one"),
+        TWO(2, "two"),
+        THREE(3, "three"),
+        FOUR(4, "four"),
+        FIVE(5, "five"),
+        SIX(6, "six"),
+        SEVEN(7, "seven"),
+        EIGHT(8, "eight"),
+        NINE(9, "nine");
+
+        companion object {
+            fun matchAt(line: String, index: Int): Int {
+                return entries.firstOrNull { line.startsWith(it.text, index) }?.value ?: -1
+            }
+        }
     }
 }
